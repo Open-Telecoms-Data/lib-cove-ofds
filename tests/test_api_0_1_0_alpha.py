e816a2f5822135b54981056cecab35191b23e669
James <james.baster@opendataservices.coop>
2022-10-04 16:11:45 +0100

    Start

---
+++ tests/test_api_0_1_0_alpha.py
@@ -0,0 +1,48 @@
+import json
+import os
+import tempfile
+
+from libcoveofds.api import ofds_json_output
+
+
+def test_basic_1():
+
+    cove_temp_folder = tempfile.mkdtemp(
+        prefix="lib-cove-ofds-tests-", dir=tempfile.gettempdir()
+    )
+    json_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "0_1_0_alpha",
+        "basic_1.json",
+    )
+
+    results = ofds_json_output(cove_temp_folder, json_filename)
+
+    assert results["schema_version"] == "0.1.0-alpha"
+    assert results["validation_errors_count"] == 0
+
+
+def test_validation_errors_1():
+
+    cove_temp_folder = tempfile.mkdtemp(
+        prefix="lib-cove-ofds-tests-", dir=tempfile.gettempdir()
+    )
+    json_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "0_1_0_alpha",
+        "validation_errors_1.json",
+    )
+
+    results = ofds_json_output(cove_temp_folder, json_filename)
+
+    assert results["schema_version"] == "0.1.0-alpha"
+
+    assert results["validation_errors_count"] == 1
+
+    validation_error: dict = json.loads(results["validation_errors"][0][0])
+    assert (
+        validation_error["message"]
+        == "'nodes' should be a JSON array. Check that value(s) appear within square brackets, [...]"
+    )
