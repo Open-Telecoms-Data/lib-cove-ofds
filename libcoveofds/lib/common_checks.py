f0cb28d1bc7751b2afdb0a38a915464ff1b38f95
James <james.baster@opendataservices.coop>
2022-10-06 15:16:33 +0100

    checks: Add link_start_node_not_found and link_end_node_not_found
    https://github.com/Open-Telecoms-Data/lib-cove-ofds/issues/1

--- libcoveofds/lib/common_checks.py
+++ libcoveofds/lib/common_checks.py
@@ -1,4 +1,140 @@
-def process_additional_checks(json_data, lib_cove_ofds_config, schema_object):
-    additional_checks = []
-    statistics = {}
+from libcoveofds.config import LibCoveOFDSConfig
+from libcoveofds.schema import SchemaOFDS
+
+
+class AdditionalCheckForNetwork:
+    """Any check or statistic that wants to be provided should extend this abstract class and overwrite methods"""
+
+    def __init__(
+        self, lib_cove_ofds_config: LibCoveOFDSConfig, schema_object: SchemaOFDS
+    ):
+        self._additional_check_results: list = []
+        self.lib_cove_ofds_config: LibCoveOFDSConfig = lib_cove_ofds_config
+        self._schema_object: SchemaOFDS = schema_object
+
+    def check_node_first_pass(self, node: dict):
+        pass
+
+    def check_link_first_pass(self, link: dict):
+        pass
+
+    def check_phase_first_pass(self, phase: dict):
+        pass
+
+    def check_organisation_first_pass(self, organisation: dict):
+        pass
+
+    def check_contract_first_pass(self, contract: dict):
+        pass
+
+    def check_node_second_pass(self, node: dict):
+        pass
+
+    def check_link_second_pass(self, link: dict):
+        pass
+
+    def check_phase_second_pass(self, phase: dict):
+        pass
+
+    def check_organisation_second_pass(self, organisation: dict):
+        pass
+
+    def check_contract_second_pass(self, contract: dict):
+        pass
+
+    def get_additional_check_results(self):
+        return self._additional_check_results
+
+
+class LinksMustHaveValidNodesAdditionalCheckForNetwork(AdditionalCheckForNetwork):
+    def __init__(self, lib_cove_bods_config, schema_object):
+        super().__init__(lib_cove_bods_config, schema_object)
+        self._node_ids_seen = []
+
+    def check_node_first_pass(self, node: dict):
+        id = node.get("id")
+        if id:
+            self._node_ids_seen.append(id)
+
+    def check_link_second_pass(self, link: dict):
+        link_id = link.get("id")
+        start = link.get("start")
+        if start:
+            self._check_node_id_valid(start, "link_start_node_not_found", link_id)
+        end = link.get("end")
+        if end:
+            self._check_node_id_valid(end, "link_end_node_not_found", link_id)
+
+    def _check_node_id_valid(self, node_id, error_type, link_id):
+        if not node_id in self._node_ids_seen:
+            self._additional_check_results.append(
+                {"type": error_type, "missing_node_id": node_id, "link_id": link_id}
+            )
+
+
+ADDITIONAL_CHECK_CLASSES_FOR_NETWORK = [
+    LinksMustHaveValidNodesAdditionalCheckForNetwork,
+]
+
+
+def process_additional_checks(
+    json_data: dict, lib_cove_ofds_config: LibCoveOFDSConfig, schema_object: SchemaOFDS
+):
+    additional_checks: list = []
+    statistics: dict = {}
+
+    # For each Network
+    networks = json_data.get("networks")
+    if isinstance(networks, list):
+        for network in networks:
+            if isinstance(network, dict):
+                additional_check_instances = [
+                    x(lib_cove_ofds_config, schema_object)
+                    for x in ADDITIONAL_CHECK_CLASSES_FOR_NETWORK
+                ]
+                nodes = network.get("nodes", [])
+                nodes = nodes if isinstance(nodes, list) else []
+                links = network.get("links", [])
+                links = links if isinstance(links, list) else []
+                phases = network.get("phases", [])
+                phases = phases if isinstance(phases, list) else []
+                organisations = network.get("organisations", [])
+                organisations = organisations if isinstance(organisations, list) else []
+                contracts = network.get("contracts", [])
+                contracts = contracts if isinstance(contracts, list) else []
+                # First pass
+                for additional_check_instance in additional_check_instances:
+                    for node in nodes:
+                        additional_check_instance.check_node_first_pass(node)
+                    for link in links:
+                        additional_check_instance.check_link_first_pass(link)
+                    for phase in phases:
+                        additional_check_instance.check_phase_first_pass(phase)
+                    for organisation in organisations:
+                        additional_check_instance.check_organisation_first_pass(
+                            organisation
+                        )
+                    for contract in contracts:
+                        additional_check_instance.check_contract_first_pass(contract)
+                # Second pass
+                for additional_check_instance in additional_check_instances:
+                    for node in nodes:
+                        additional_check_instance.check_node_second_pass(node)
+                    for link in links:
+                        additional_check_instance.check_link_second_pass(link)
+                    for phase in phases:
+                        additional_check_instance.check_phase_second_pass(phase)
+                    for organisation in organisations:
+                        additional_check_instance.check_organisation_second_pass(
+                            organisation
+                        )
+                    for contract in contracts:
+                        additional_check_instance.check_contract_second_pass(contract)
+                # Results
+                for additional_check_instance in additional_check_instances:
+                    additional_checks.extend(
+                        additional_check_instance.get_additional_check_results()
+                    )
+
+    # Return
     return {"additional_checks": additional_checks, "statistics": statistics}
--- tests/test_api_0_1_0_alpha.py
+++ tests/test_api_0_1_0_alpha.py
@@ -21,6 +21,7 @@ def test_basic_1():
 
     assert results["schema_version"] == "0.1.0-alpha"
     assert results["validation_errors_count"] == 0
+    assert results["additional_checks_count"] == 0
 
 
 def test_validation_errors_1():
@@ -46,3 +47,49 @@ def test_validation_errors_1():
         validation_error["message"]
         == "'nodes' should be a JSON array. Check that value(s) appear within square brackets, [...]"
     )
+
+    assert results["additional_checks_count"] == 0
+
+
+def test_start_node_not_found_1():
+
+    cove_temp_folder = tempfile.mkdtemp(
+        prefix="lib-cove-ofds-tests-", dir=tempfile.gettempdir()
+    )
+    json_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "0_1_0_alpha",
+        "start_node_not_found_1.json",
+    )
+
+    results = ofds_json_output(cove_temp_folder, json_filename)
+
+    assert results["schema_version"] == "0.1.0-alpha"
+
+    assert results["validation_errors_count"] == 0
+
+    assert results["additional_checks_count"] == 1
+    assert results["additional_checks"][0]["type"] == "link_start_node_not_found"
+
+
+def test_end_node_not_found_1():
+
+    cove_temp_folder = tempfile.mkdtemp(
+        prefix="lib-cove-ofds-tests-", dir=tempfile.gettempdir()
+    )
+    json_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "0_1_0_alpha",
+        "end_node_not_found_1.json",
+    )
+
+    results = ofds_json_output(cove_temp_folder, json_filename)
+
+    assert results["schema_version"] == "0.1.0-alpha"
+
+    assert results["validation_errors_count"] == 0
+
+    assert results["additional_checks_count"] == 1
+    assert results["additional_checks"][0]["type"] == "link_end_node_not_found"
