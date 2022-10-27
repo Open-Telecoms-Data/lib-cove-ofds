7b8fe2c61f4d11a04e96fdaace6139a2cefba5a1
James <james.baster@opendataservices.coop>
2022-10-27 12:38:18 +0100

    config: Add root_list_path
    https://github.com/Open-Telecoms-Data/lib-cove-ofds/issues/10

--- libcoveofds/config.py
+++ libcoveofds/config.py
@@ -8,6 +8,7 @@ LIB_COVE_OFDS_CONFIG_DEFAULT.update(
         "package_schema_url": "https://raw.githubusercontent.com/Open-Telecoms-Data/open-fibre-data-standard/b2b4ed722d6b078251da05eb0da8304ebefd34e5/schema/network-package-schema.json",
         "schema_host": "https://raw.githubusercontent.com/Open-Telecoms-Data/open-fibre-data-standard/b2b4ed722d6b078251da05eb0da8304ebefd34e5/schema/",
         "schema_version": "0.1.0-alpha",
+        "root_list_path": "networks",
     }
 )
 
