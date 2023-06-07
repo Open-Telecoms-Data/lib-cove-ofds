fb5a7007ba348beb8a5f4602fc53ed9e134a200a
James B <james.baster@opendataservices.coop>
2023-06-07 08:58:31 +0100

    geojsontojson:  jsontogeojson: support featureType
    https://github.com/Open-Telecoms-Data/lib-cove-ofds/issues/73
    https://github.com/Open-Telecoms-Data/lib-cove-ofds/issues/21

--- tests/test_geojson_to_json.py
+++ tests/test_geojson_to_json.py
@@ -3,7 +3,7 @@ import os
 
 import pytest
 
-from libcoveofds.geojson import GeoJSONToJSONConverter
+from libcoveofds.geojson import GeoJSONAssumeFeatureType, GeoJSONToJSONConverter
 
 GEOJSON_TO_JSON_FILES = [
     # basic example
@@ -20,6 +20,8 @@ GEOJSON_TO_JSON_FILES = [
     ("network_inconsistent_1"),
     # bad example
     ("bad_1"),
+    # Old data with no feature type. Converter should use assumed_feature_type setting and still work.
+    ("no_featuretype_1"),
 ]
 
 
@@ -59,7 +61,65 @@ def test_geojson_to_json(filename):
         span_data = json.load(fp)
 
     converter = GeoJSONToJSONConverter()
-    converter.process_data(nodes_data, span_data)
+    converter.process_data(
+        nodes_data, assumed_feature_type=GeoJSONAssumeFeatureType.NODE
+    )
+    converter.process_data(
+        span_data, assumed_feature_type=GeoJSONAssumeFeatureType.SPAN
+    )
+
+    with open(expected_filename) as fp:
+        expected_data = json.load(fp)
+    assert expected_data == converter.get_json()
+
+    with open(meta_expected_filename) as fp:
+        meta_expected_data = json.load(fp)
+    assert meta_expected_data == converter.get_meta_json()
+
+
+def test_geojson_to_json_when_told_wrong_assumed_feature_type():
+    """Test basic_1 converts ok when we tell the converter to assume the wrong feature type.
+    As this data specifies feature type, the converter should take the correct feature type
+    from the data and ignore the assumption setting."""
+    nodes_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "geojson_to_json",
+        "basic_1.nodes.geo.json",
+    )
+    spans_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "geojson_to_json",
+        "basic_1.spans.geo.json",
+    )
+    expected_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "geojson_to_json",
+        "basic_1.expected.json",
+    )
+    meta_expected_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "geojson_to_json",
+        "basic_1.meta.expected.json",
+    )
+
+    with open(nodes_filename) as fp:
+        nodes_data = json.load(fp)
+    with open(spans_filename) as fp:
+        span_data = json.load(fp)
+
+    converter = GeoJSONToJSONConverter()
+    # Wrong type!
+    converter.process_data(
+        nodes_data, assumed_feature_type=GeoJSONAssumeFeatureType.SPAN
+    )
+    # Wrong type again, so clumsy!
+    converter.process_data(
+        span_data, assumed_feature_type=GeoJSONAssumeFeatureType.NODE
+    )
 
     with open(expected_filename) as fp:
         expected_data = json.load(fp)
